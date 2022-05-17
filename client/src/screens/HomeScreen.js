a8483d5e955149f3c690004abcae81005435dd45
Dennisoshak <dennisoshak@gmail.com>
2022-05-17 21:52:18 +0300

    rating

--- client/src/screens/HomeScreen.js
+++ client/src/screens/HomeScreen.js
@@ -18,9 +18,9 @@ const HomeScreen = () => {
       <h1>Latest Products</h1>
       <Row>
         {products.map((product) => (
-          <Row sm={12} md={6} lg={4} xl={3} key={product._id}>
+          <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
             <Product product={product} />
-          </Row>
+          </Col>
         ))}
       </Row>
     </>
