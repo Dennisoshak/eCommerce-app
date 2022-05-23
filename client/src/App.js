8345bcf9271e1bebcf7f6518ec9e1b87a7944be2
MeirRozenbaum <97239582+MeirRozenbaum@users.noreply.github.com>
2022-05-23 21:20:12 +0300

    login

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import CartScreen from "./screens/CartScreen.js";
 import ProductScreen from "./screens/ProductScreen";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Container } from "react-bootstrap";
+import LoginScreen from "./screens/LoginScreen.js";
 
 function App() {
   return (
@@ -15,10 +16,11 @@ function App() {
         <Container>
           <Routes>
             <Route path="/" element={<HomeScreen />} />
+            <Route path="/login" element={<LoginScreen />} />
             <Route path="/cart/:id" element={<CartScreen />} />
             <Route path="/cart" element={<CartScreen />} />
             <Route path="/product/:id" element={<ProductScreen />} />
-            <Route path="/cart/:id?" element={<CartScreen />} />
+            {/* <Route path="/cart/:id?" element={<CartScreen />} /> */}
           </Routes>
         </Container>
       </main>
