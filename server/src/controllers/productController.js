e298a68d077a261bc9bb580d5182c32eb1f4293d
alanlipschutz <alanelipschutz@gmail.com>
2022-05-25 10:20:37 +0300

    create and update product in back

--- server/src/controllers/productController.js
+++ server/src/controllers/productController.js
@@ -27,4 +27,46 @@ const deleteProduct = asyncHandler(async (req, res) => {
   }
 });
 
-export { getProducts, getProductById, deleteProduct };
+const createProduct = asyncHandler(async (req, res) => {
+  const product = await Product.create({
+    name: "Sample name",
+    price: 0,
+    user: req.user._id,
+    image: "/images/sample.jpg",
+    brand: "Sample Brand",
+    category: "Sample category",
+    countInStock: 0,
+    numReviews: 0,
+    description: "Sample Description",
+  });
+  // const createdProduct = await product.save();
+  res.status(201).json({ product });
+});
+
+const updateProduct = asyncHandler(async (req, res) => {
+  const { name, price, description, image, brand, category, countInStock } =
+    req.body;
+  const product = await Product.findById(req.params.id);
+  if (product) {
+    (product.name = name),
+      (product.price = price),
+      (product.description = description),
+      (product.image = image),
+      (product.brand = brand),
+      (product.category = category),
+      (product.countInStock = countInStock);
+    const updateProduct = await product.save();
+    res.status(200).json({ updateProduct });
+  } else {
+    res.status(404);
+    throw new Error("Product not found");
+  }
+});
+
+export {
+  getProducts,
+  getProductById,
+  deleteProduct,
+  createProduct,
+  updateProduct,
+};
