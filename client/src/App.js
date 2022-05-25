155b137bb177fdec9f9faa6b31e8eb986acf49e1
alanlipschutz <alanelipschutz@gmail.com>
2022-05-25 11:48:46 +0300

    change sending product in back. updateProduct page implemented without functionality

--- client/src/App.js
+++ client/src/App.js
@@ -16,6 +16,7 @@ import UserListScreen from "./screens/UserListScreen";
 import OrderScreen from "./screens/OrderScreen.js";
 import UserEditScreen from "./screens/UserEditScreen";
 import ProductListScreen from "./screens/ProductListScreen.js";
+import ProductEditScreen from "./screens/ProductEditScreen.js";
 
 function App() {
   return (
@@ -30,6 +31,10 @@ function App() {
             <Route path="/placeorder" element={<PlaceOrderScreen />} />
             <Route path="/admin/userlist" element={<UserListScreen />} />
             <Route path="/admin/user/:id/edit" element={<UserEditScreen />} />
+            <Route
+              path="/admin/product/:id/edit"
+              element={<ProductEditScreen />}
+            />
             <Route path="/admin/productlist" element={<ProductListScreen />} />
             <Route path="/login" element={<LoginScreen />} />
             <Route path="/register" element={<RegisterScreen />} />
