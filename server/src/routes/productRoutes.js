895ee22e1b705625ac0154a6ba518a5edfad3334
MeirRozenbaum <97239582+MeirRozenbaum@users.noreply.github.com>
2022-05-25 14:20:45 +0300

    update

--- server/src/routes/productRoutes.js
+++ server/src/routes/productRoutes.js
@@ -1,16 +1,5 @@
 import express from "express";
 const router = express.Router();
-<<<<<<< HEAD
-import {getProducts, getProductById, createProductReview} from '../controllers/productController.js'
-import { protect } from "../middleware/authMiddleware.js";
-
-// Fetch all products
-
-router.route("/products").get(getProducts);
-router.route("/products/:id/reviews").post(protect, createProductReview);
-router.route("/products/:id").get(getProductById);
-
-=======
 import { protect, admin } from "../middleware/authMiddleware.js";
 import {
   getProducts,
@@ -18,14 +7,17 @@ import {
   deleteProduct,
   updateProduct,
   createProduct,
+  createProductReview
 } from "../controllers/productController.js";
-
 // Fetch all products
+
 router.route("/").get(getProducts).post(protect, admin, createProduct);
+router.route("/:id/reviews").post(protect, createProductReview);
 router
   .route("/:id")
   .get(getProductById)
   .delete(protect, admin, deleteProduct)
   .put(protect, admin, updateProduct);
->>>>>>> 834d7a531fa660f34d000d38d279a4d201e09117
+
+
 export default router;
